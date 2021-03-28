60b2501fc6f2ddcd006dc6bc0bbf402aa0daf0cf
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-03-28 11:28:24 +0800

    updated table with just text

--- static/js/app.js
+++ static/js/app.js
@@ -3,25 +3,56 @@
 d3.json("./data/samples.json").then((importedData) => {
     // console.log(importedData);
     var data = importedData;
-// console.log(data);
-// console.log(data.metadata);
-metadata = data.metadata;
-idNo = metadata.map(obj=>obj.id);
-//console.log(idNo);
-ethnic = metadata.map(obj=>obj.ethnicity);
-//console.log(ethnic);
-gender = metadata.map(obj=>obj.gender);
-// console.log(gender);
-age = metadata.map(obj=>obj.age);
-// console.log(age);
-loc = metadata.map(obj=>obj.location);
-// console.log(loc); 
-bbtype = metadata.map(obj=>obj.bbtype);
-// console.log(bbtype);
-wfreq = metadata.map(obj=>obj.wfreq);
-// console.log(wfreq);
+    // console.log(data);
+    metadata = data.metadata;
+    console.log(data.metadata);
+    idNo = metadata.map(obj=>obj.id);
+    // console.log(idNo);
+    ethnic = metadata.map(obj=>obj.ethnicity);
+    // console.log(ethnic);
+    gender = metadata.map(obj=>obj.gender);
+    // console.log(gender);
+    age = metadata.map(obj=>obj.age);
+    // console.log(age);
+    loc = metadata.map(obj=>obj.location);
+    // console.log(loc); 
+    bbtype = metadata.map(obj=>obj.bbtype);
+    // console.log(bbtype);
+    wfreq = metadata.map(obj=>obj.wfreq);
+    // console.log(wfreq);
+});
 
- });
+// var data = [];
+
+//     //append to the html code as list: id="sample-metadata"
+    info_table = d3.select("#sample-metadata");
+//     //remove everything in the sample-metadata
+    info_table.html("");
+//     //Extract out the data 
+    // for (i=0,i<idNo.length,i++)
+    // if (dataset == idno[i]){    
+
+
+//     //Append data into the table 
+    row = info_table.append("p")
+    // var row = div.append("ul");
+    row.text("id:");
+    row = info_table.append("p")
+    row.text("ethnicity:");
+    row = info_table.append("p")
+    row.text("gender:");
+    row = info_table.append("p")
+    row.text("age:");
+    row = info_table.append("p")
+    row.text("location:");
+    row = info_table.append("p")
+    row.text("bbtype:");
+    row = info_table.append("p")
+    row.text("wfreq:");
+
+    // }
+// }
+
 
 
 // // Initializes the page with a default plot
